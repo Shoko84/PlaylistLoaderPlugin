8ede2564c3cb37bc9ca44cfef36fe9b92870d6ca
Rithik Bhatia <rithikbhatia12@gmail.com>
2020-02-07 18:16:55 -0500

    Setup flowcontroller to include a viewcontroller

--- PlaylistLoaderPlugin/UI/PlaylistsFlowCoordinator.cs
+++ PlaylistLoaderPlugin/UI/PlaylistsFlowCoordinator.cs
@@ -1,12 +1,39 @@
-﻿using HMUI;
+﻿using BeatSaberMarkupLanguage;
+using HMUI;
+using System;
 
 namespace PlaylistLoaderPlugin.UI
 {
     public class PlaylistsFlowCoordinator : FlowCoordinator
     {
+        private NavigationController _playlistsNavigationController;
+        private PlaylistsViewController _playlistsViewController;
+        public void Awake()
+        {
+            _playlistsViewController = BeatSaberUI.CreateViewController<PlaylistsViewController>();
+            _playlistsNavigationController = BeatSaberUI.CreateViewController<NavigationController>();
+        }
         protected override void DidActivate(bool firstActivation, ActivationType activationType)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                if (firstActivation)
+                {
+                    showBackButton = true;
+                    ProvideInitialViewControllers(_playlistsNavigationController, _playlistsViewController);
+                    title = "Playlist Manager";
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.log.Error(ex);
+            }
+        }
+
+        protected override void BackButtonWasPressed(ViewController topViewController)
+        {
+            var mainFlow = BeatSaberMarkupLanguage.BeatSaberUI.MainFlowCoordinator;
+            mainFlow.DismissFlowCoordinator(this);
         }
     }
 }
---
+++ PlaylistLoaderPlugin/UI/ViewController/PlaylistsViewController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+using BeatSaberMarkupLanguage.ViewControllers;
+
+namespace PlaylistLoaderPlugin.UI
+{
+    public class PlaylistsViewController : BSMLResourceViewController
+    {
+        public override string ResourceName => "PlaylistLoaderPlugin.UI.BSML.PlaylistsView.bsml";
+    }
+}
